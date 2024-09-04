84c158f1f9d0b3cceb7bb10f670f8380916ce5ef
fred42vid <fred42vid@gmail.com>
2024-09-04 18:31:34 +0200

    read gallery items from JSON object, then assemble the HTML code

--- source/js/main.js
+++ source/js/main.js
@@ -47,48 +47,11 @@ const galleryJson = `
   }
 }`;
 
-const itemBlob = `
-              <div class="card shadow-sm">
-                <svg class="bd-placeholder-img card-img-top"
-                     width="100%" height="225"
-                     xmlns="http://www.w3.org/2000/svg"
-                     role="img" aria-label="Placeholder: Thumbnail"
-                     preserveAspectRatio="xMidYMid slice"
-                     focusable="false">
-                  <title>Placeholder</title>
-                  <rect width="100%" height="100%" fill="#55595c"/>
-                  <text x="50%" y="50%" fill="#eceeef" dy=".3em">
-                    Thumbnail
-                  </text>
-                </svg>
-                <div class="card-body">
-                  <p class="card-text">
-                    This is a wider card with supporting text
-                    below as a natural lead-in to additional
-                    content. This content is a little bit longer.
-                  </p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button"
-                              class="btn btn-sm btn-outline-secondary">
-                        View
-                      </button>
-                      <button type="button"
-                              class="btn btn-sm btn-outline-secondary">
-                        Edit
-                      </button>
-                    </div>
-                    <small class="text-body-secondary">
-                      9 mins
-                    </small>
-                  </div>
-                </div>
-              </div>
-`;
-
 function fillGallery() {
 
-  const noOfItems = 10;
+  const galleryObj = JSON.parse(galleryJson);
+    // TODO: not yet error checking on the JSON object; it's still static
+  const noOfItems = galleryObj["gallery"]["items"].length;
 
   delayFor(initialDelay);
   if (noOfItems < 1) {
@@ -115,9 +78,47 @@ function fillGallery() {
 
     for (let i = 0; i < noOfItems; i++) {
       delayFor(iterationDelay);
-      var itemDiv = document.createElement("div");
+      let galItem = galleryObj["gallery"]["items"][i];
+      let id = galItem["id"];
+      let name = galItem["name"];
+      let imgurl = galItem["imgurl"];
+      let description = galItem["description"];
+      
+      let itemDiv = document.createElement("div");
       itemDiv.setAttribute("class", "col");
-      itemDiv.innerHTML = itemBlob;
+      let itemHTMLcode = `
+              <div class="card shadow-sm" id="card_${id}">
+                <img class="card-img-top"
+                     src="${imgurl}"
+                     alt="Picture of ${name}">
+                <div class="card-body">
+                  <h5 class="card-title">${name}</h5>
+                  <p class="card-text">
+                    ${description}
+                  </p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button"
+                              class="btn btn-sm btn-outline-secondary">
+                        View
+                      </button>
+                      <button type="button"
+                              class="btn btn-sm btn-outline-secondary">
+                        Edit
+                      </button>
+                    </div>
+                    <small class="text-body-secondary">
+                      9 mins
+                    </small>
+                  </div>
+                </div>
+              </div>
+      `;
+        // composing the child elements piece by piece should be faster
+        // than having a large string assigned to x.innerHTML;
+        // however, working with a large string (as done here) is
+        // easier to overview
+      itemDiv.innerHTML = itemHTMLcode;
       wrapperDiv.appendChild(itemDiv);
     }
   }
